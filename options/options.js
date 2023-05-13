c119ad82cf0dd1b8c3909787c130470de82fcbb4
Crowdbotics <team@crowdbotics.com>
2023-05-13 00:33:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "isync-fitness-40667",
+  url: "https://isync-fitness-40667.botics.co",
+  api: "https://isync-fitness-40667.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
